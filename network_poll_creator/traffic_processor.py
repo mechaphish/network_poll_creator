5a7f3d58f4222adf289441662e7394076d57fd1d
Aravind, Machiry <machiry@cs.ucsb.edu>
2016-06-11 14:32:21 -0700

    Refactoring

--- network_poll_creator/traffic_processor.py
+++ network_poll_creator/traffic_processor.py
@@ -1,83 +1,30 @@
 import os
 import pickle
-from farnsworth.actions import CQE_POV, Data, Write, Read, Match
+from farnsworth.actions import CFE_POLL, Data, Write, Read, Match
+from common_utils.pcap_parser import InputData, OutputData
 import logging
+import binascii
 import collections
 
 l = logging.getLogger("network_poll_creator.traffic_processor")
 
 
-class NetworkData(object):
-
-    def __init__(self, target_data):
-        """
-
-        :param target_data:
-        :return:
-        """
-        self.target_data = target_data
-        self._is_input = False
-        self._is_output = False
-
-    @property
-    def is_input(self):
-        """
-
-        :return:
-        """
-        return self._is_input
-
-    @property
-    def is_output(self):
-        """
-
-        :return:
-        """
-        return self._is_output
-
-    @property
-    def data(self):
-        """
-
-        :return:
-        """
-        return self.target_data
-
-
-class InputData(NetworkData):
-    """
-
-    """
-
-    def __init__(self, target_data):
-        NetworkData.__init__(self, target_data)
-        self._is_input = True
-
-
-class OutputData(NetworkData):
-    """
-
-    """
-
-    def __init__(self, target_data):
-        NetworkData.__init__(self, target_data)
-        self._is_output = True
-
-
 class PollData(object):
     """
-
+        Object representing interaction of one challenge set.
     """
+
+    RANDOM_SEED_LENGTH = 96
 
     def __init__(self, cs_id, connection_id):
         self.cs_id = cs_id
         self.connection_id = connection_id
         self.data_elements = {}
 
-    def to_cqe_xml(self):
+    def to_cfe_xml(self):
         """
-
-        :return:
+            Convert the current Poll Data in to CFE Poll and return the same
+        :return: CFE Poll xml as string.
         """
         target_actions = []
         ordered_dict = collections.OrderedDict(sorted(self.data_elements.items()))
@@ -93,15 +40,19 @@ class PollData(object):
         poll_header = """<?xml version="1.0" standalone="no" ?>
                         <!DOCTYPE pov SYSTEM "/usr/share/cgc-docs/replay.dtd">
                     """
-        # create CQE POV Object
-        poll = CQE_POV(str(self.cs_id), target_actions)
+        # create random seed, this may / will be changed in the process
+        # to create a valid CFE POLL
+        rand_seed = binascii.b2a_hex(os.urandom(PollData.RANDOM_SEED_LENGTH))
+        rand_seed = rand_seed[0:PollData.RANDOM_SEED_LENGTH]
+        # create CFE POLL Object
+        poll = CFE_POLL(str(self.cs_id), rand_seed, target_actions)
         # return the final Xml.
         return poll_header + str(poll)
 
 
 class TrafficProcessor(object):
     """
-
+        Processes the raw pickled data
     """
     INPUT_SIDE = 'client'
 
@@ -110,37 +61,37 @@ class TrafficProcessor(object):
 
     def get_polls(self):
         """
+        Processes the provided pickled file and returns list of PollData objects.
 
-        :return:
+        :return: list of poll data objects
         """
         # No polls
         to_ret = []
         poll_cache = {}
         if os.path.exists(self.pickled_file):
+            l.info("Trying to read pickled file:" + str(self.pickled_file))
             fp = open(self.pickled_file, 'rb')
+            # Try to load pickled data until End of File
             while True:
                 try:
+                    # load pickled data
                     csid, connection_id, msg_id, side, message = pickle.load(fp)
                     cache_key = str(csid) + ',' + str(connection_id)
                     if cache_key not in poll_cache:
+                        # create a new Poll
                         poll_cache[cache_key] = PollData(csid, connection_id)
                     curr_poll_data = poll_cache[cache_key]
                     if side == TrafficProcessor.INPUT_SIDE:
                         network_data = InputData(message)
                     else:
                         network_data = OutputData(message)
+                    # add data into correct poll
                     curr_poll_data.data_elements[msg_id] = network_data
                 except EOFError as e:
                     break
             fp.close()
             to_ret = list(poll_cache.values())
+            l.info("Got:" + str(len(to_ret)) + " Polls.")
+        else:
+            l.error("Provided pickled file:" + str(self.pickled_file) + " does not exist.")
         return to_ret
-
-target_fp = '/home/machiry/Downloads/raw_pickled_traffic'
-new_tp = TrafficProcessor(target_fp)
-all_polls = new_tp.get_polls()
-for curr_p in all_polls:
-    curr_x = curr_p.to_cqe_xml()
-    fp = open(str(curr_p.cs_id) + '_' + str(curr_p.connection_id) + '.xml', 'w')
-    fp.write(curr_x)
-    fp.close()
